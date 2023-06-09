838e975fdfc327d8c90cb43c8eca9b34611d4878
BackGwa <64456214+BackGwa@users.noreply.github.com>
2023-06-09 15:13:16 +0900

    PHP

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html lang="ko">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>계급사회</title>
+
+        <!-- 웹 어플리케이션 등록 -->
+        <link rel="manifest" href="./manifest.json">
+        <link rel="shortcut icon" href="./res/icons/favicon.ico" type="image/x-icon">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <link rel="apple-touch-icon" href="./res/icons/icon.png">
+
+        <!-- CSS 적용 -->
+        <link rel="stylesheet" href="./style/base.css">
+    </head>
+
+    <body>
+        <!-- 앱 UI -->
+        <div class="app"></div>
+        <!-- 하단 독 추가 -->
+        <?php include './dock.html' ?>
+    </body>
+</html>
