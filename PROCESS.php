73da464fef11b80d089f89adc4b77fca837cfae6
BackGwa <backgwa@icloud.com>
2023-06-10 23:03:40 +0900

    점수 처리 백엔드

---
+++ PROCESS.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>계급사회</title>
+</head>
+
+<body>
+    <?php
+
+        $uid = $_POST['select_uid'];
+        $score = $_POST['rank'];
+        $other = $_POST['other'];
+
+        $result = (int)$score + (int)$other;
+
+        $DB = mysqli_connect("localhost", "root", "");
+        mysqli_select_db($DB, "rank");
+
+        $query = "update board set score = (score + $result) where uid = $uid";
+        $result = mysqli_query($DB, $query);
+        mysqli_close($DB);
+
+    ?>
+    <script>
+        alert("점수를 완벽하게 등록했어요!");
+        history.back();
+    </script>
+</body>
+
+</html>
